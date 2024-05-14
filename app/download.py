f4866f63b227027c75a95c831a2f4ddbeb05b880
Kamil Biktimirov <Biktimirov.kamil@yandex.ru>
2024-05-14 13:37:08 +0300

    Added command handling for URL

--- app/download.py
+++ app/download.py
@@ -1,16 +1,27 @@
 import logging
 
+import click
 from downloaders import YandexLinksDownloader
 
-URL = "https://browser.yandex.ru/tab-groupings-share/5fd5ec7b-56cd-4cd4-bdcf-bc44535fb1f5"
-# "https://browser.yandex.ru/tab-groupings-share/0c455cac-be2c-48c7-8a5f-c1323e49f808"
+# URL = "https://browser.yandex.ru/tab-groupings-share/5fd5ec7b-56cd-4cd4-bdcf-bc44535fb1f5"
+# # "https://browser.yandex.ru/tab-groupings-share/0c455cac-be2c-48c7-8a5f-c1323e49f808"
 
-if __name__ == "__main__":
 
-    logger = logging.Logger("DOWNLOAD", level=logging.INFO)
+@click.command()
+@click.option("--url", help="URL to download from")
+def download(url: str) -> None:
+    if not url:
+        click.echo("You must provide '--url' argument")
+        return
 
-    downloader = YandexLinksDownloader(URL)
+    downloader = YandexLinksDownloader(url)
+
+    click.echo("Downloading started...")
 
     downloader.download()
 
-    logger.info("Downloading process successfully finished")
+    click.echo("Downloading finished successfully")
+
+
+if __name__ == "__main__":
+    download()
