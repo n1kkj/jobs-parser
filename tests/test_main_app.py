307fc9cf8dd3e47d71e726c8af5950833ebb5ce4
n1kkj <nikita.lakin.topka@gmail.com>
2024-09-27 00:21:00 +0300

    fixing tests

--- tests/test_main_app.py
+++ tests/test_main_app.py
@@ -5,4 +5,4 @@ from urls_crowler.run_crowlers import run_crowlers_threading
 
 class TestMainApp(unittest.IsolatedAsyncioTestCase):
     async def test_main_app(self):
-        await run_crowlers_threading()
+        await run_crowlers_threading(1334928287)
