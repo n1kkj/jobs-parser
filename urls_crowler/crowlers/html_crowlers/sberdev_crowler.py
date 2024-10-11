9d3a7525b0ed14034c70e6c3f09b4b09089413ff
n1kkj <nikita.lakin.topka@gmail.com>
2024-10-12 01:58:47 +0300

    fixed sberdev

--- urls_crowler/crowlers/html_crowlers/sberdev_crowler.py
+++ urls_crowler/crowlers/html_crowlers/sberdev_crowler.py
@@ -1,9 +1,11 @@
 from urls_crowler.crowlers.base_url_crowler import BaseHTMLUrlCrowler
 from urls_crowler.parsers import SberDevParser
+from urls_crowler.utils import GetDataClass
 
 
 class SberDevCrowler(BaseHTMLUrlCrowler):
     main_ulr = 'https://developers.sber.ru/kak-v-sbere/vacancies?city=moscow'
+    data_get_function = GetDataClass.get_html_data_by_scrolling
     vacancies_prefix = 'https://developers.sber.ru/'
     html_link_class = 'sc-14149c6a-1 dZZpyw'
     links_params = {}
--- urls_crowler/parsers/html_parsers/sberdev_parser.py
+++ urls_crowler/parsers/html_parsers/sberdev_parser.py
@@ -3,9 +3,9 @@ from urls_crowler.parsers import BaseHTMLUrlParser
 
 class SberDevParser(BaseHTMLUrlParser):
     title_key = 'h1|sc-45e1da05-0 sc-81d0f9f1-7 QPtSW kFPzMP'
-    desc_key = 'div|sc-iGgWBj sc-45e1da05-25 sc-26c5ab91-3 faWaSL XArFl iNiqbL'
+    desc_key = 'div|sc-dc74419e-0 gTvbPQ'
     salary_key = None
     exp_key = 'div|sc-81d0f9f1-12 jsWPdY|1'
     city_key = 'div|sc-81d0f9f1-12 jsWPdY|0'
-    employer_key = 'span|sc-fd3988f5-10 gcZxDC'
+    employer_key = None
     work_format_key = None
