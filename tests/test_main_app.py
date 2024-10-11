a2eeba7db2e75b0bc6f54495dd4c243ad2b80f05
n1kkj <nikita.lakin.topka@gmail.com>
2024-10-11 22:28:52 +0300

    deleted unused tests

--- tests/test_main_app.py
+++
@@ -1,8 +0,0 @@
-import unittest
-
-from urls_crowler.run_crowlers import run_crowlers_threading
-
-
-class TestMainApp(unittest.IsolatedAsyncioTestCase):
-    async def test_main_app(self):
-        await run_crowlers_threading(1334928287)
