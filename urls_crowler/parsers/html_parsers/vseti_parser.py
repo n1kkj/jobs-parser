c374e09e0a8a29a770edada4accd1da4d46c2974
n1kkj <nikita.lakin.topka@gmail.com>
2024-10-18 17:19:44 +0300

    vseti no desc

--- urls_crowler/parsers/html_parsers/vseti_parser.py
+++ urls_crowler/parsers/html_parsers/vseti_parser.py
@@ -3,7 +3,7 @@ from urls_crowler.parsers import BaseHTMLUrlParser
 
 class VsetiParser(BaseHTMLUrlParser):
     title_key = 'h1|heading-9'
-    desc_key = 'div|rich-text-block w-richtext'
+    desc_key = None
     salary_key = 'p|skills1-in-job|-1'
     exp_key = 'p|skills1-in-job'
     city_key = None
