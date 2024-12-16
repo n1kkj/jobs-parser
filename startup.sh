83a079d872cbdc01bb05fd4c7207f1d4a5cd9cdc
n1kkj <nikita.lakin.topka@gmail.com>
2024-12-16 21:10:08 +0300

    telegram code bot

--- startup.sh
+++ startup.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 
-gunicorn yo_parser_bot:app --bind 0.0.0.0:8001 --worker-class uvicorn.workers.UvicornWorker &
-gunicorn get_tg_code:code_app --bind 0.0.0.0:8005 --worker-class uvicorn.workers.UvicornWorker
+gunicorn yo_parser_bot:app --bind 0.0.0.0:8001 --worker-class uvicorn.workers.UvicornWorker
